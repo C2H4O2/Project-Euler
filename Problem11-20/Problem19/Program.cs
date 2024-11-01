fc49af6d9fc8ec1ef5997ef026298402e3e8a08e
C2H4O2 <ngethanyk@gmail.com>
2024-11-01 15:32:50 +0000

    Problem 21

--- Problem11-20/Problem19/Program.cs
+++ Problem11-20/Problem19/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class Problem1
+public class Problem19
 {
-    //If we list all the natural numbers below $10$ that are multiples of $3$ or $5$, we get $3, 5, 6$ and $9$. The sum of these multiples is $23$.</p>
-    //Find the sum of all the multiples of $3$ or $5$ below $1000$.</p>
     public static void Main(string[] args)
     {
         Console.WriteLine(SumOfDays());
